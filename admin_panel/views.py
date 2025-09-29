3521b4a5659c3ca877c384e444720b77f737e5d8
donjo <donjorois@gmail.com>
2025-09-30 00:07:03 +0530

    added superadmin dashboard view

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from accounts.utils import role_check
+from django.http import HttpResponseForbidden
+from accounts.models import CustomUser
+from tasks.models import Task   
 
-# Create your views here.
+
+@login_required
+def superadmin_dashboard(request):
+    if not role_check(request.user, ['superadmin']):
+        return HttpResponseForbidden("Forbidden")
+    
+
+    
+    users = CustomUser.objects.all()
+    tasks = Task.objects.all()
+    return render(request, 'admin_panel/superadmin_dashboard.html',{
+        'users': users,
+        'tasks': tasks
+    })
+
+
+
