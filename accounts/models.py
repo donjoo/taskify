9434974843152fd804ec1c8afb1276c8b185cf86
donjo <donjorois@gmail.com>
2025-09-29 23:49:41 +0530

    properly saved user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('superadmin', 'SuperAdmin'),
+        ('admin', 'Admin'),
+        ('user', 'User'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+    def is_superadmin(self):
+        return self.role == 'superadmin'
+
+    def is_admin(self):
+        return self.role == 'admin'
