9a596e972e4ce5bfb49202b03b49529e11300214
donjo <donjorois@gmail.com>
2025-09-30 02:58:36 +0530

    login-logout superadmin dashboard working

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'), 
     path('logout/', views.logout_view, name='logout'),
 
-    path('superadmin/dashboard/', views.superadmin_dashboard, name='superadmin-dashboard'),
+    path('superadmin/dashboard/', views.superadmin_dashboard, name='superadmin_dashboard'),
 ]
