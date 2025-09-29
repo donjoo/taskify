fdb3efc106571397cad8e42c3873ceebde3ad1d6
donjo <donjorois@gmail.com>
2025-09-29 21:40:25 +0530

    added TaskUpdateView

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from .models import Task
 from .serializers import TaskSerializer, TaskStatusUpdateSerializer
 from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 
 
@@ -14,4 +15,16 @@ class TaskListView(generics.ListAPIView):
         return Task.objects.filter(assigned_to = self.request.user)
     
 
+class TaskUpdateView(generics.UpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskStatusUpdateSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_url_kwarg = 'id'
+
+
+    def get_object(self):
+        obj = get_object_or_404(Task, id=self.kwargs.get('id'))
+        if obj.assigned_to != self.request.user:
+           self.permission_denied(self.request, message="You do not have persmission to edit this task.")
+        return obj
     
