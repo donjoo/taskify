f01627bdc31b2bf5e8e2a8db88192351f1b05d2a
donjo <donjorois@gmail.com>
2025-09-29 23:16:45 +0530

    added doc-strings

--- tasks/views.py
+++ tasks/views.py
@@ -7,16 +7,23 @@ from rest_framework.response import Response
 from rest_framework import status
 
 def _user_role(user):
+    """Helper function to get user role"""
     return getattr(user, 'role', None)
 
 
 class IsAdminOrSuperAdmin(permissions.BasePermission):
+    """
+    Custom permission to allow only users with role 'admin' or 'superadmin'.
+    """
     def has_permission(self, request, view):
         return _user_role(request.user) in ('admin', 'superadmin') 
 
 
 
 class TaskListView(generics.ListAPIView):
+    """
+    API endpoint to list all the tasks assigned to the loggedd-in user.
+    """
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
@@ -25,6 +32,11 @@ class TaskListView(generics.ListAPIView):
     
 
 class TaskUpdateView(generics.UpdateAPIView):
+    """
+    API endpoint for users to update the status of their assigned tasks.
+    User can mark a task as completed and must provide a completion report and worked hours.
+    serialier checks wheather completion report and worked hours are provided.
+    """
     queryset = Task.objects.all()
     serializer_class = TaskStatusUpdateSerializer
     permission_classes = [IsAuthenticated]
@@ -40,12 +52,22 @@ class TaskUpdateView(generics.UpdateAPIView):
 
 
 class TaskReportView(generics.RetrieveAPIView):
+    """
+    Api endpoint for admins and superadmin to view task completion report
+    ONly for tast with status "completed"
+
+
+    """
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated, IsAdminOrSuperAdmin]
     lookup_url_kwarg = 'id'
 
 
     def get_object(self):
+        """
+        
+        Retrieve task obj by id and checks if its status is "completed"
+        """
         obj = get_object_or_404(Task, id=self.kwargs.get('id'))
         if obj.status != 'completed':
             self.permission_denied(self.request, message="Report only savailable for completed tasks.")
@@ -53,6 +75,10 @@ class TaskReportView(generics.RetrieveAPIView):
     
 
     def get(self, request, *args, **kwargs):
+        """
+        
+        return the completion report and worked hours  
+        """
         task = self.get_object()
         data = {
             'id': task.id,
