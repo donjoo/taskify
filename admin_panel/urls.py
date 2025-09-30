fc74aed661a294190b4104aee34e0f709bbccbff
donjo <donjorois@gmail.com>
2025-09-30 15:30:03 +0530

    added userCURD url,firm

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('superadmin/dashboard/', views.superadmin_dashboard, name='superadmin_dashboard'),
     path('admin/dashboard/', views.admin_dashboard, name='admin_dashboard'),
 
+    # User CRUD (SuperAdmin only)
+    path("users/create/", views.user_create, name="user_create"),
+    path("users/<int:pk>/update/", views.user_update, name="user_update"),
+    path("users/<int:pk>/delete/", views.user_delete, name="user_delete"),
+
+
 ]
