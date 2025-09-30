9709752eaa62aff488c6df05a9cf89df2927ce8a
donjo <donjorois@gmail.com>
2025-09-30 22:28:47 +0530

    fixed createsuperuser

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,28 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, role='user', **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, role=role, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        # Set role to superadmin automatically
+        return self.create_user(username, email, password, role='superadmin', **extra_fields)
+
+
+
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
