02117d8c79046990320131c5f6dcdb4829a8b06d
donjo <donjorois@gmail.com>
2025-09-30 15:32:26 +0530

    properly saved forms

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import CustomUser
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput,required=False)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'role']
+
+        def save(self,commit=True):
+            user = super().save(commit=False)
+            if self.cleaned_data['password']:
+                user.set_password(self.cleaned_data['password'])
+            if commit:
+                user.save()
+            return user
